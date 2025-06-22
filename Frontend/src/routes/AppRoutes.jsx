bceeec78249a0078fe707fe8e9e73c37b16d6df1
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-06-22 18:30:27 +0530

    AppRoute.jsx file do

---
+++ Frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import ProductDetails from "../pages/ProductDetails";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import UserDashboard from "../pages/UserDashboard";
+import AdminDashboard from "../pages/AdminDashboard";
+import ProtectedRoute from "../components/ProtectedRoute";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/checkout" element={<Checkout />} />
+      
+      {/* Protected Routes */}
+      <Route path="/dashboard/user" element={<ProtectedRoute role="user"><UserDashboard /></ProtectedRoute>} />
+      <Route path="/dashboard/admin" element={<ProtectedRoute role="admin"><AdminDashboard /></ProtectedRoute>} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
